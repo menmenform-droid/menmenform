016956673e7e8c6b98e44c2ed147eda65d143537
menmenform-droid <menmenform@gmail.com>
2026-03-04 13:41:25 +0800

    Add files via upload

---
+++ setting.js
@@ -0,0 +1,7 @@
+const SETTINGS = {
+    BRANCH_NAME: "MBS WORLD", // Contoh cawangan semasa
+    GAS_URL: "https://script.google.com/macros/s/AKfycbxe6l1PA8kTTm3rCAAKazNQRyCvAnLeuFopYjGbfkEAbwfRlyAHwfXu8C11ZhACr_8p0g/exec", // Masukkan URL GAS anda
+    CONSIGNMENT_VENDORS: ["VENDOR A", "VENDOR B", "GARDENIA"], // Jika pilih ini, auto jadi Consignment
+    ALL_VENDORS: ["VENDOR A", "VENDOR B", "GARDENIA", "NESTLE", "MAMEE", "OTHER"],
+    TRANSIT_METHODS: ["WAREHOUSE TRANSIT POINT", "VENDOR SELF PICKUP"]
+};
